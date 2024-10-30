3eee6689d2a9aa587c97a19ea5c32f1966885b39
maiducanha1anh <maiducanha1anh@gmail.com>
2024-10-30 15:53:00 +0700

    hello add

---
+++ 30-10-2024/api_mysql/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const todosRouter = require('./src/routers/todos');
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+})
+app.use('/todos', todosRouter);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
